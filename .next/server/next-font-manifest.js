cef236029816e5dc3ef33b359acc023bbb4a9832
tetref <sunilcutit08@gmail.com>
2026-04-22 10:49:48 +0900

    remove build directory and change gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ashutoshmishra/Desktop/instance-inspector-main-main/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
